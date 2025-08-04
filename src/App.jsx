ae1c3881bb8b285aef23e0056a6cc819d9c56c55
Andressa Leite <dre_tl@hotmail.com>
2025-08-04 18:46:25 -0300

    inicializando projeto

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import Tasks from "./components/Tasks";
+import AddTasks from "./components/AddTasks";
+
+function App() {
+  const [tasks, setTasks] = useState();
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+      </div>
+      <Tasks />
+      <AddTasks />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AddTasks.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AddTasks() {
+  return <div></div>;
+}
+
+export default AddTasks;

9aae3edc3f58d43db3e277ebe16bb599135d2c78
Andressa Leite <dre_tl@hotmail.com>
2025-08-06 14:36:19 -0300

    criacao do componente Tasks

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,37 @@ import Tasks from "./components/Tasks";
 import AddTasks from "./components/AddTasks";
 
 function App() {
-  const [tasks, setTasks] = useState();
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description:
+        "Estudar programação para se tornar um desenvolvedor Full Stack",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar inglês",
+      description: "Estudar inglês para se tornar fluente",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar matemática",
+      description: "Estudar matemática",
+      isCompleted: false,
+    },
+  ]);
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[800px]">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
+        <Tasks tasks={tasks} />
+        <AddTasks />
       </div>
-      <Tasks />
-      <AddTasks />
     </div>
   );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,7 +1,21 @@
+import { ChevronRightIcon } from "lucide-react";
 import React from "react";
 
-function Tasks() {
-  return <div></div>;
+function Tasks(props) {
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button className="bg-slate-400 w-full text-left text-blue-950 p-2 rounded-md">
+            {task.title}
+          </button>
+          <button className="bg-slate-400">
+            <ChevronRightIcon />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Tasks;

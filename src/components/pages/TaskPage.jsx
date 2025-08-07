80cb963e328df01d17d763e583ee6efccb839f8b
Andressa Leite <dre_tl@hotmail.com>
2025-08-07 19:35:43 -0300

    Estilizada a pagina de DETALHES DA TAREFA

--- src/components/pages/TaskPage.jsx
+++ src/components/pages/TaskPage.jsx
@@ -1,3 +1,4 @@
+import { ChevronLeftIcon } from "lucide-react";
 import React from "react";
 import { useSearchParams } from "react-router-dom";
 
@@ -7,8 +8,20 @@ function TaskPage() {
   const description = searchParams.get("description");
   return (
     <div className="h-screen w-screen bg-slate-500 p-6">
-      <h1>{title}</h1>
-      <p>{description}</p>
+      <div className="w-[800px] space-y-4 ">
+        <div className="flex justify-center relative mb-6">
+          <button className="absolute left-0 bottom-0">
+            <ChevronLeftIcon />
+          </button>
+          <h1 className="text-3xl text-slate-300 font-bold text-center">
+            Detalhes da Tarefa
+          </h1>
+        </div>
+        <div className="bg-slate-300 p-4 rounded-md">
+          <h2 className="text-xl text-blue-950 font-bold">{title}</h2>
+          <p className="text-blue-950">{description}</p>
+        </div>
+      </div>
     </div>
   );
 }

ba5b50b16390a3cffff3a88122b51a363142a0c8
Andressa Leite <dre_tl@hotmail.com>
2025-08-06 21:55:02 -0300

    trocando as props pelo destructuring

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,13 +1,13 @@
 import { ChevronRightIcon, Trash } from "lucide-react";
 import React from "react";
 
-function Tasks(props) {
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-      {props.tasks.map((task) => (
+      {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
           <button
-            onClick={() => props.onTaskClick(task.id)}
+            onClick={() => onTaskClick(task.id)}
             className={`bg-slate-400 w-full text-left text-blue-950 p-2 rounded-md ${
               task.isCompleted && "line-through"
             }`}
@@ -17,7 +17,7 @@ function Tasks(props) {
           <button className="bg-slate-400">
             <ChevronRightIcon />
           </button>
-          <button onClick={() => props.onDeleteTaskClick(task.id)}>
+          <button onClick={() => onDeleteTaskClick(task.id)}>
             <Trash />
           </button>
         </li>

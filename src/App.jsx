eebd1f2c072242fadfec1215c4c3d6a609843c7a
Andressa Leite <dre_tl@hotmail.com>
2025-08-08 11:44:59 -0300

    retirei a lista de tarefas que inicializavam na pagina

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,7 @@ import { v4 } from "uuid";
 
 function App() {
   const [tasks, setTasks] = useState(
-    JSON.parse(localStorage.getItem("tasks")) || [
-      {
-        id: 1,
-        title: "Estudar programação",
-        description:
-          "Estudar programação para se tornar um desenvolvedor Full Stack",
-        isCompleted: false,
-      },
-      {
-        id: 2,
-        title: "Estudar inglês",
-        description: "Estudar inglês para se tornar fluente",
-        isCompleted: false,
-      },
-      {
-        id: 3,
-        title: "Estudar matemática",
-        description: "Estudar matemática",
-        isCompleted: false,
-      },
-    ]
+    JSON.parse(localStorage.getItem("tasks")) || []
   );
 
   useEffect(() => {

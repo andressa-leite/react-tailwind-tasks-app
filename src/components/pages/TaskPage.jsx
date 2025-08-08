f7499ccd4c0149b53dc566f0b59eea12ed034749
Andressa Leite <dre_tl@hotmail.com>
2025-08-08 10:19:55 -0300

    funcao para voltar para a pagina inicial

--- src/components/pages/TaskPage.jsx
+++ src/components/pages/TaskPage.jsx
@@ -1,8 +1,9 @@
 import { ChevronLeftIcon } from "lucide-react";
 import React from "react";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function TaskPage() {
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const title = searchParams.get("title");
   const description = searchParams.get("description");
@@ -10,7 +11,10 @@ function TaskPage() {
     <div className="h-screen w-screen bg-slate-500 p-6">
       <div className="w-[800px] space-y-4 ">
         <div className="flex justify-center relative mb-6">
-          <button className="absolute left-0 bottom-0">
+          <button
+            onClick={() => navigate(-1)}
+            className="absolute left-0 bottom-0"
+          >
             <ChevronLeftIcon />
           </button>
           <h1 className="text-3xl text-slate-300 font-bold text-center">
